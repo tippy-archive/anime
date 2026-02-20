0bddcf9a218fe3fd96a48c507dfedc902046c497
tippy-archive <tippy-archive@gmail.com>
2026-02-20 23:58:39 +0900

    test

--- test/_data/23.js
+++ test/_data/23.js
@@ -1,27 +1,28 @@
-window.globalData = window.globalData || [];
-
-(async function() {
+(function() {
     const currentScript = document.currentScript;
     const parentContainer = currentScript.parentElement;
+    const jsonUrl = '_data/23.json';
 
-    try {
-        const response = await fetch('_data/23.json');
-        const data = await response.json();
+    window.dataLoader = window.dataLoader.then(async () => {
+        try {
+            const response = await fetch(jsonUrl);
+            const data = await response.json();
 
-        window.globalData.push(...data);
+            window.globalData.push(...data);
 
-        parentContainer.innerHTML = data
-            .filter(item => !item.i.includes('data:image/svg+xml'))
-            .map(item => `
-        <a href="${item.u}">
-            <ul class="list-select playlist-main">
-                <li class="list-img"><img src="${item.i}" loading="lazy"/></li>
-                <li class="list-title"><p>${item.t}</p></li>
-            </ul>
-        </a>
-    `).join('');
+            parentContainer.innerHTML = data
+                .filter(item => !item.i.includes('data:image/svg+xml'))
+                .map(item => `
+                    <a href="${item.u}">
+                        <ul class="list-select playlist-main">
+                            <li class="list-img"><img src="${item.i}" loading="lazy"/></li>
+                            <li class="list-title"><p>${item.t}</p></li>
+                        </ul>
+                    </a>
+                `).join('');
 
-    } catch (error) {
-        console.error("데이터 로드 실패:", error);
-    }
+        } catch (error) {
+            console.error(`${jsonUrl} 로드 실패:`, error);
+        }
+    });
 })();
