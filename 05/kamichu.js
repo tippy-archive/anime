6aa4eb0f133ac0a0cabbc8e48150b25cb8f1a55d
tippy-archive <tippy-archive@gmail.com>
2026-03-07 12:34:18 +0900

    anime

--- 05/kamichu.js
+++ 05/kamichu.js
@@ -6,7 +6,22 @@ var currentActiveImageId = 'image1';
 
 var randomimages = new Array()
 
-
+randomimages[0] = "https://lh3.googleusercontent.com/pw/AP1GczN4FDB63_cufbDoGGTe8Vx1hvaveLpcPjUYIl75ONay4cEwEWV9ocQBDKPB9cU8J0hoIbLGy9YRuKPp7hZ1m1WzvmrI_f2ev6oHOEaU97fGr-qxkX5xL4OX_XoQlCN8iTNGAq14smc4aRLaXd_zG8xa=w1920-h1080"
+randomimages[1] = "https://lh3.googleusercontent.com/pw/AP1GczMZEytPL9CutPbbyI0Lm0WoHoO85KXjorcGxbitTnAq2dCMGYgLnthH48f5t-5zvDh1pRTuGJMROo4x_rcX2ooRsw1tY_QVPjDqGaXNSRN38DHcpuB2XfAatNuoz5Q2Y1PlzRqbDXmTWf_91xojWUnR=w1920-h1080"
+randomimages[2] = "https://lh3.googleusercontent.com/pw/AP1GczPz9NiSA3wWBupns3kflxfBzsVfZjKQlvEUnzafQF1he8IPU84QPuZyNE14yXqjpZJ8AVWcvD7x641UhyXGtClj82k_i_zX1Tudiyv-osml61pdCbcKE9pGXc-IC2sPog7w9QcT9ZseV5saGkEUFFi6=w1920-h1080"
+randomimages[3] = "https://lh3.googleusercontent.com/pw/AP1GczOdyrSzQ546uIpYPGj5lAGP-_0kqRil1cFO_7JXIHw-oEA3T-2BzLHg391gHFI4_vBnB_WS_nBMmo1YKG1CcHHm9Q7gc3K5CpXHEhTIq_nq0klxUAOxTJBHTZZ5uZ0JAgAQaW22Qfg4Omt8zvCMgZPM=w1920-h1080"
+randomimages[4] = "https://lh3.googleusercontent.com/pw/AP1GczPnMyiuiAeJRoY-UFZpYLdgph3gaE6wllC8F9pCGpQbOFLiv6JjT70KSBDCL7-Gpab90mILkzrgJWoLRMKhnGTAWYpelg7ZrErAp_ZtDuy7CDbXhY-28AKKFP7d-diTHC5ZQuMjMGiYag3EPFhLTZbJ=w1920-h1080"
+randomimages[5] = "https://lh3.googleusercontent.com/pw/AP1GczMU4RYqNCIc6SZLY23VWFERi6_YuLtpzn8z9EbFOOrLQTQkAn58qSTy23KzYaVPDQsvsQvMk7mR96QneNHxZdcVXomkadz-lMj-oay95g7adhl767zITzE7Nncy4A_4-5KutLwkhHiuugv-RCYNPqlw=w1920-h1080"
+randomimages[6] = "https://lh3.googleusercontent.com/pw/AP1GczNLc1NEIiiS4nPBtDYKiK_dT47XBqV8udmPQdWRVi5tFXlMEHzznZaTcjd6IPqDZvb6zbjrhQMvbNOs9WG043NjrJbugsV1cR_lwpLSLHHZzHlhqaR_3zCMtoOWEfXt-cxufObxR76tx3Z4lK_sYRv0=w1920-h1080"
+randomimages[7] = "https://lh3.googleusercontent.com/pw/AP1GczMbyPHQd9d6gia0u4GX8QfLWLr2SfjuFnEgWDlVkJPQMxGQTjRLuG0LpXRcqrIyyv2x65_kEOn8-e9UtIHXMDVLpFmsUH5br-I6z_WN1KxmmJIm3e_OfGi2f2vaZfUWjmhMVlwKyvFS2R-WVNAj1Uj3=w1920-h1080"
+randomimages[8] = "https://lh3.googleusercontent.com/pw/AP1GczPZcLf0yBm3hoD71sWzmFMiBIFikJHJr09Fi2SvOeo8JzCBIarXArEZFhLfyO9UJylIBFP5LabGT9MPc2SZq3ItO8HxvPlzA4a2mhpscEYOlFYvbsIs2yk1_sLVbLT0fPc2QiCKd5oXsJZ7VO6s886N=w1920-h1080"
+randomimages[9] = "https://lh3.googleusercontent.com/pw/AP1GczOXegj7A_iXhc2Wus_JzXmfINR4gTMRrfN7bO27WMHkn0ROkgJJGsdLuyf9RX8gr7h3oFK11q3J2l0pTBkrFldGuGs2ONaUOWWKtLlf-5Cj2Kmc2K2u8ekmtdshIS48kdBz_a269npjhvDBNmPxxbaI=w1920-h1080"
+randomimages[10] = "https://lh3.googleusercontent.com/pw/AP1GczPdTjBfv9rUzV0zwBi2lNZEyN-mb-QbOB3uCjDp8Y41zbIW2FrVKqGw7Fr8CJZ5q4F0vBFNkVgoOUYF5Azmqbb8oN_H9mTmnCz76xa5LR402313efcdiIX2LMT-jsOiNpfHCBEyNK6afuLqhYxA65nQ=w1920-h1080"
+randomimages[11] = "https://lh3.googleusercontent.com/pw/AP1GczP_zpximYnIOXP5GnHH4PR26cJcuO2642Ucfu0bOxaU1N0XQgBGUSKHh3drl-DOH4SF-1vRdCJBqdVRjWdJMJQpEOTcIE7ZfACwXPqgeP4TNcjlIxTaan9GapCFWRl_1p1U3FKNwBVW4axUWS_U6shN=w1920-h1080"
+randomimages[12] = "https://lh3.googleusercontent.com/pw/AP1GczOBsSUWNaqF8kgnQGQedADz9HcdEuWSPOilkG3v6vw8-sQTELVPflGrudRNrr6wSoaNarWdgR158DCIj1jhR_cpJgNZzUBRwfCrhTF9brecGbF8Wtu7SwOkk6WMzLWSo3C5XZx_dwPBYZ1XuigQss_2=w1920-h1080"
+randomimages[13] = "https://lh3.googleusercontent.com/pw/AP1GczMrRikZB9fOgsYAM-FFBRN7FiTVXu8e71ktFFETEct63zbrNEdUfwGfARIfRfM-IoTt0LPLeK0FlYsjr5_695AWgouvYr_wWaW5SjV6dVQvc0iOAWKCsp7yMNmwCH50vdMJCIABnwTQbFroSo87ZlY_=w1920-h1080"
+randomimages[14] = "https://lh3.googleusercontent.com/pw/AP1GczMxW2kg8zL3XIFKzZyDRvs_lQ2lrmJ3qS7_VxJkO-pWN3bmTYiY1ZKVBiz1jSQLHdBCfpxEjYIPEEngNslekmgoOBGBHGcUDM-sFbDXho3T-voX6beFkT_G6aCuN8IKd4P47Fr9FbqRctQ9Ub7gVh9s=w1920-h1080"
+randomimages[15] = "https://lh3.googleusercontent.com/pw/AP1GczO5AjC2WGT9NMPHQTlMHXgUM8crOCPwD1DUtdQuFSeZa-cL5uS2FVqNUkF9ms0xagqUDnnUrZ2vaI9UcbK61x-s3QL1qEQpWkhKa0x9GvMv25r8ofeJcwKoHINcQngTBVsS3OPIlizOsxmEBYvBMQLa=w1920-h1080"
 
 var initialImageIndex = Math.floor(Math.random() * randomimages.length);
 document.getElementById('image1').src = randomimages[initialImageIndex];
--- 05/rozen-02.js
+++ 05/rozen-02.js
@@ -6,7 +6,20 @@ var currentActiveImageId = 'image1';
 
 var randomimages = new Array()
 
-
+randomimages[0] = "https://lh3.googleusercontent.com/pw/AP1GczMKBuiCXur-pOo3c1tH-6lfA7Y6nA0lBxUVppK5xPI58gFMwWssMlAYGa_ey8BbJ9o9Siemu358nZjfqRZ1bQX3dpA_4CYrs64idv9OqG-k-4LJZftglzRzE4lN3Hq4z26f_5v_URzAwtptKRVHCP-4=w1920-h1080"
+randomimages[1] = "https://lh3.googleusercontent.com/pw/AP1GczNr8aryjJqWiO6co5-o4CAPfc_Lvl5qIgF54Fa9Bs2C6mS_kng4mfo_oVaECtGYxy_DnS2cVwnWF8rGcNMd9wXVMQB9DMT1dYdt7IEPVApHcAqbgFeMFQxH8RhCIiIDFqEu1OGVeuTgDgJQIRp2NGOg=w1920-h1080"
+randomimages[2] = "https://lh3.googleusercontent.com/pw/AP1GczOPooHZH-NPgXGVN5TbR6erbwolSGaJxKgl8Oc7fQl2uYSX5dixSvLOuVQU8BPYSSHqh2XQ9xglPYdJ11RAQpwK00kDoJKVw4XMyHQCeHLPcJdVii93_xsHIQME05Nn8FYpTPlFsvvMcQerAQiRe5Jl=w1920-h1080"
+randomimages[3] = "https://lh3.googleusercontent.com/pw/AP1GczOacqyyXjlXYMlbVRjtgrII0OR1cgbgCkCua-GQEdkcWPr7iDIfVR9Lki_dti_XWL-sr8oAJ32ALKTco1kXqGgXsEjHLlYXdLkGrzPKqYqIFQyfsak1tWZFIGOR6iaQtrJDs_G33drnKXhA6UYgOeIe=w1920-h1080"
+randomimages[4] = "https://lh3.googleusercontent.com/pw/AP1GczMpiKqc7E972jJoIbCMnrxcAeYaxhyE59mzMinTqxRZcn3DGexbOnE3QO05fL18p4SWEWma4g56CURFzfvHUinhMeFCyhp_QyzogkQFbzJpYPvheVUw4NOiJ-OA1P0p7Xkhx8R8hwUg433jLuuf0me9=w1920-h1080"
+randomimages[5] = "https://lh3.googleusercontent.com/pw/AP1GczNVi5--wzn-Z-dj-HIzM-jOvKPOiKnCkPDF0ihLy8nizlWnjKIRmynRKVKJkWGYubvuSjYJyR40fo8u2e2EuLFZ0ahCWyrGj1gZBX2Wm9VMJJw-lZQig6wAl46h3OgtXkhGBrypQ0hUGiA7OS9QU5ws=w1920-h1080"
+randomimages[6] = "https://lh3.googleusercontent.com/pw/AP1GczOFbBJAJP3dk-275T1K-GuavvSOy0ZgivD9aqSsarWABfq-DNXyWedt1C9BXSvPeDsbDrQltcS45dx4SBGe3VYKr9o0XKjhOHjaRKxuCET9-Upg6s-KuyQ-id9PfXLnJQIzZpHBf6_7x4aW3x-xkHSV=w1920-h1080"
+randomimages[7] = "https://lh3.googleusercontent.com/pw/AP1GczPrQ35FB23C3kgzT-_laFGy6dtMm_cSDQ5DEVK1G7Mi0wgRzKFIghtDGF8-eA_zxzmCx1tnfN_YsrSOPoAQi0omCNlZRa_adTyhbynlBnu0eyosHUmaG26M9JUZA-Pp3XHYizUdmTrYtq7hMoGOW8ou=w1920-h1080"
+randomimages[8] = "https://lh3.googleusercontent.com/pw/AP1GczP2rWx4NXuYtIyP__SIIIYCTPdomD1MSLhObeGy291HCkyWzLBY5cGIq35jiza8MTpjiZf3VlKD2cANl216GZ0wmtNOH5yrxOk3LOjBfbVZk92EKcnSVyS7a9YXAVXBv6rXTPrsdGbDQMT6Uimija9m=w1920-h1080"
+randomimages[9] = "https://lh3.googleusercontent.com/pw/AP1GczOuKe6N1WcAxXed1-Gm8iekdLIAPieq_4vGEhgMZkuy6tIOEh_C0v4DfMUr3fIPw_ORFXOM5h2x6CG050FzTxza76DRx15_6KsV4VET16GDDy4OS4WMP4lVxEMCBCN3IfCvTrhzNNooBg_xQfQI0AuS=w1920-h1080"
+randomimages[10] = "https://lh3.googleusercontent.com/pw/AP1GczNSII1LxRi9LdTq5zUiYEWvZ0KPL2WvvINeO86_ChLaOpuwbSQtxF4t2X-hyROAgbXQTgfAElxNOS-VVDdueoFGkmPWn5-g2Vgr8VZ-62ukrqAWcDx3jNS_I6goeR2823s9qYRKtZv5QDaFbBgv3YUj=w1920-h1080"
+randomimages[11] = "https://lh3.googleusercontent.com/pw/AP1GczOkr76N5fSwPrJC3Mhj6gxl-bqzlTn7RdzYvEM2VcdSYb9t__Adb1vvkqnQ2thHIlqc9nNkJUzgkMwtXoWP5kLbzoLWfP92tVaND7P2v0ABR8rvt-yg_ztFexz8SFmFVoDb256D10ZwvO6VOPOCJd62=w1920-h1080"
+randomimages[12] = "https://lh3.googleusercontent.com/pw/AP1GczMHWnED3pOR1fwRi6SItVc-N1oS07Fw4m-mOtyRN-LWOTWg3OT7CL4Dmyz5S77oh7uOr_JN_gsIYyU8bv53dha2qzMShAMpEogmtsAbtf2Uhflshnc1mRCXIJ0qW1AxnES59cTb9V5aGle913Hpk0TU=w1920-h1080"
+randomimages[13] = "https://lh3.googleusercontent.com/pw/AP1GczNhWftBDj6AGmMOLzZCbjFHIqkhmHplGkUwU4REM_9v8ecIXVZh24AkveAi5hn1cFqGaF4A7sC6bmv7_tGVZLSqRy1u82ao4GLyI4oQjbcK26O_FET33hbXiPzGBtOPGuLjICe4mbodj_6_n0dau-3H=w1920-h1080"
 
 var initialImageIndex = Math.floor(Math.random() * randomimages.length);
 document.getElementById('image1').src = randomimages[initialImageIndex];
